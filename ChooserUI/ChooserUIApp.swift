fd3ce4b8385218bc811a6bf5aa85af94c4ebb85c
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2021-09-10 11:33:51 +0200

    Added ChooserUI target

---
+++ ChooserUI/ChooserUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooserUIApp.swift
+//  ChooserUI
+//
+//  Created by Armin Briegel on 2021-09-10.
+//
+
+import SwiftUI
+
+@main
+struct ChooserUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
